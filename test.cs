c68371b094aca211a457b50707ed7fd84f32e58e
Nate VanBuskirk <nathan.vanbuskirk@seriussoft.com>
2015-08-30 20:15:54 -0500

    not going to use p4merge...need a better merge tool that acknowledges and highlights different file types or at least acknowledges the idfference between a comment, whitespace, formatting, and syntax...

--- test.cs
+++ test.cs
@@ -1,11 +1,14 @@
-public namespace Tests
-{
-  public class Test1
-  {
-    public void TestReturnsTrue()
-    {
-      System.Diagnostics.Debug.Assert(true, "Uh oh, the systems are failing!");
-      
-    }
-  }
-}
+//public namespace Tests
+//{
+  //public class Test1
+  //{
+    ///// <summary>
+    ///// Should not error!!!
+    ///// </summary>
+    //public void TestReturnsTrue()
+    //{
+      //System.Diagnostics.Debug.Assert(true, "Uh oh, the systems are failing!");
+      //
+    //}
+  //}
+//}
